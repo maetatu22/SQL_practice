8e8e756954378aa24f35c36d0bcef520b4ee884f
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-13 11:11:10 +0900

    カラム変更

--- -- CREATE TABLE goods (id INT, name VARC.sql
+++ -- CREATE TABLE goods (id INT, name VARC.sql
@@ -1,3 +1,7 @@
 -- CREATE TABLE goods (id INT, name VARCHAR(255))
 SHOW TABLES;
 SHOW columns FROM goods;
+-- ALTER TABLE goods ADD (price int , zaiko int);
+SHOW columns FROM goods;
+-- ALTER TABLE goods CHANGE zaiko stock int;
+SHOW columns FROM goods;
