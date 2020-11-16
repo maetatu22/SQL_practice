bcd9ac92513d44d84665e8370156f8611964e5d4
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-16 10:24:36 +0900

    カラム を指定した登録

--- USE sqltest;.sql
+++ USE sqltest;.sql
@@ -1,5 +1,7 @@
 -- USE sqltest;
 -- SHOW DATABASES;
 USE sqltest;
-INSERT INTO goods VALUES(2, "消しゴム", 150);
+-- INSERT INTO goods VALUES(2, "消しゴム", 150);
+-- SELECT * FROM goods;
+INSERT INTO goods(id, name) VALUES(5, "定規");
 SELECT * FROM goods;
