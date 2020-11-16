b150ee378673fd061ba5d53e0b25eb31f4cb3665
maetatu22 <zbn.ex.zz@gmail.com>
2020-11-16 10:26:40 +0900

    UPDATE練習

--- USE sqltest;.sql
+++ USE sqltest;.sql
@@ -3,5 +3,8 @@
 USE sqltest;
 -- INSERT INTO goods VALUES(2, "消しゴム", 150);
 -- SELECT * FROM goods;
-INSERT INTO goods(id, name) VALUES(5, "定規");
-SELECT * FROM goods;
+-- INSERT INTO goods(id, name) VALUES(5, "定規");
+-- SELECT * FROM goods;
+
+-- UPDATE goods SET price = 100 WHERE id = 2;
+-- SELECT * FROM goods;
